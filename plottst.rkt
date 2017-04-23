b4a04373e5ea4c8a7ab33cc5116dea228b61c330
Devon Hills <devon_hills@student.uml.edu>
2017-04-23 19:41:17 -0400

    improve implementation

--- plottst.rkt
+++ plottst.rkt
@@ -11,13 +11,13 @@
 ;; placeholder test values
 
 (define cycle_temperatures
-  (list 1 2 3 4 5 6 7 8 9 10))
+  (list 10 20 30 40 50 60 70 80 90 100))
 
 (define cycle_moistures
-  (list 1 2 3 4 5 6 7 8 9 10))
+  (list 100 200 300 400 500 600 700 800 900 1000))
   
 (define cycle_lights
-  (list 1 2 3 4 5 6 7 8 9 10))
+  (list 10 20 30 40 50 60 70 80 90 100))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
@@ -33,56 +33,78 @@
 ;;to cycle-time MAX),
 
 
+#|
+
 ;;temperatures from 0 - 100
 
 (define (plot-cycle-temperatures)
-   (parameterize ([plot-width    150]
-                  [plot-height   150]
-                  [plot-x-label  "time"]
-                  [plot-y-label  "temperature"])
-     (plot
-      (points (for/list ([i cycle_temperatures] [j (in-range (length
-cycle_temperatures))])
-                (list j i)))
-      #:x-min 0 #:x-max (+ 1 (length cycle_temperatures)) #:y-min 0 #:y-max
-100))
-)
+  (parameterize ([plot-width    500]
+                 [plot-height   500]
+                 [plot-x-label  "time"]
+                 [plot-y-label  "temperature"])
+    (plot
+     (points (for/list ([i cycle_temperatures]
+                        [j (in-range (length cycle_temperatures))])
+               (list j i)))
+     #:x-min 0 #:x-max (+ 1 (length cycle_temperatures)) #:y-min 0 #:y-max 100))
+  )
 
 
 ;;moistures 0 to 1500
 
 (define (plot-cycle-moistures)
-   (parameterize ([plot-width    150]
-                  [plot-height   150]
-                  [plot-x-label  "time"]
-                  [plot-y-label  "moisture"])
-     (plot
-      (points (for/list ([i cycle_moistures] [j (in-range (length
-cycle_moistures))])
+  (parameterize ([plot-width    500]
+                 [plot-height   500]
+                 [plot-x-label  "time"]
+                 [plot-y-label  "moisture"])
+    (plot
+      (points (for/list ([i cycle_moistures]
+                         [j (in-range (length cycle_moistures))])
                 (list j i)))
-      #:x-min 0 #:x-max (+ 1 (length cycle_moistures)) #:y-min 0 #:y-max
-1000))
-)
+      #:x-min 0 #:x-max (+ 1 (length cycle_moistures)) #:y-min 0 #:y-max 1500))
+  )
+
 
 ;;lights 0 - 100
 
 (define (plot-cycle-lights)
-     (parameterize ([plot-width    150]
-                  [plot-height   150]
-                  [plot-x-label  "time"]
-                  [plot-y-label  "light"])
-     (plot-frame
-      (points (for/list ([i cycle_lights] [j (in-range (length
-cycle_lights))])
-                (list j i)))
-      #:x-min 0 #:x-max (+ 1 (length cycle_lights)) #:y-min 0 #:y-max 100))
-)
+  (parameterize ([plot-width    500]
+                 [plot-height   500]
+                 [plot-x-label  "time"]
+                 [plot-y-label  "light"])
+    (plot
+     (points (for/list ([i cycle_lights]
+                        [j (in-range (length cycle_lights))])
+               (list j i)))
+     #:x-min 0 #:x-max (+ 1 (length cycle_lights)) #:y-min 0 #:y-max 100))
+  )
+
+|#
 
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 
+;; rewrite as more generic implementation
+
+(define (plot-cycle cyclelist xlabel ylabel ymax)
+  (parameterize ([plot-width    500]
+                 [plot-height   500]
+                 [plot-x-label  xlabel]
+                 [plot-y-label  ylabel])
+    (plot
+     (points (for/list ([i cyclelist]
+                        [j (in-range (length cyclelist))])
+               (list j i)))
+     #:x-min 0 #:x-max (+ 1 (length cyclelist)) #:y-min 0 #:y-max ymax))
+  )
+
+
+
+
+;;sample call
+(plot-cycle cycle_lights "test X" "test Y" 100)
 
 
 
